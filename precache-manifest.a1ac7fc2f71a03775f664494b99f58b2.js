7c6488ff1ba387e7b8dca8766ef58f6f6fd3ca3e
Greilfang <565222945@qq.com>
2019-06-20 15:54:34 +0800

    Updates

---
+++ precache-manifest.a1ac7fc2f71a03775f664494b99f58b2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e92e9b96edadd637cf7fc8f84c77750",
+    "url": "/Air-War/72843970f1fecd3beb50.worker.js"
+  },
+  {
+    "revision": "4e7c9530d1c481e3dcc929f063f1eed2",
+    "url": "/Air-War/index.html"
+  },
+  {
+    "revision": "516848f37ec9d1a2849e",
+    "url": "/Air-War/static/css/main.6ba38414.chunk.css"
+  },
+  {
+    "revision": "a19d9d94d0e0f14a191f",
+    "url": "/Air-War/static/js/2.47668277.chunk.js"
+  },
+  {
+    "revision": "516848f37ec9d1a2849e",
+    "url": "/Air-War/static/js/main.ade09dbf.chunk.js"
+  },
+  {
+    "revision": "fbf3b3618aff2aeb85f3",
+    "url": "/Air-War/static/js/runtime~main.1c6e5457.js"
+  },
+  {
+    "revision": "d28fc70f29b3c25bf99f3b63983cffa7",
+    "url": "/Air-War/static/media/Explosion_002_Tile_8x8_256x256.d28fc70f.png"
+  },
+  {
+    "revision": "78621f638fada65d5ab24e8b4f89eb3b",
+    "url": "/Air-War/static/media/GrassGreenTexture0003.78621f63.jpg"
+  },
+  {
+    "revision": "002f20c3fa9d6ea9cd496da92c104461",
+    "url": "/Air-War/static/media/GrassGreenTexture0003_normal.002f20c3.jpg"
+  },
+  {
+    "revision": "da494a885ce544445cd09fbbde67058e",
+    "url": "/Air-War/static/media/Rock_08_UV_H_CM_1.da494a88.jpg"
+  },
+  {
+    "revision": "6085c2f7e2c6f24d53852f009cdfdbfe",
+    "url": "/Air-War/static/media/Rock_08_UV_H_CM_1_normal.6085c2f7.jpg"
+  },
+  {
+    "revision": "4c8e91a4d7aa09069938dcc87e7c7e2b",
+    "url": "/Air-War/static/media/Snow2.4c8e91a4.jpg"
+  },
+  {
+    "revision": "a33d50da063b016852d1d139cf6e73b1",
+    "url": "/Air-War/static/media/Water_1_M_Normal.a33d50da.jpg"
+  },
+  {
+    "revision": "639428cf065384aae22d01b529011992",
+    "url": "/Air-War/static/media/Water_2_M_Normal.639428cf.jpg"
+  },
+  {
+    "revision": "2f1ab236426c1d5cf582d29688d06129",
+    "url": "/Air-War/static/media/explosion1.2f1ab236.ogg"
+  },
+  {
+    "revision": "752472e3de62bb7224e84d7b7ed5d119",
+    "url": "/Air-War/static/media/explosion2.752472e3.ogg"
+  },
+  {
+    "revision": "b8ff47f4e17fb1e2d7fb36a04e29f76e",
+    "url": "/Air-War/static/media/gatling.b8ff47f4.ogg"
+  },
+  {
+    "revision": "db06d2c4c1f1985f0fe212585967e2a9",
+    "url": "/Air-War/static/media/impact1.db06d2c4.ogg"
+  },
+  {
+    "revision": "7d720129509d1a0ac532450bced731b0",
+    "url": "/Air-War/static/media/impact2.7d720129.ogg"
+  },
+  {
+    "revision": "7a5b4e82670c04e6395146cdcb07c602",
+    "url": "/Air-War/static/media/impact3.7a5b4e82.ogg"
+  },
+  {
+    "revision": "43348926f7eb827d3651c3412707b2f0",
+    "url": "/Air-War/static/media/impact4.43348926.ogg"
+  },
+  {
+    "revision": "f0faf674f89b6f143519e8529ea644dd",
+    "url": "/Air-War/static/media/missile.f0faf674.ogg"
+  },
+  {
+    "revision": "4939a8d27601adf3dd652e5703b6f679",
+    "url": "/Air-War/static/media/smokeparticle.4939a8d2.png"
+  },
+  {
+    "revision": "8cb6275163547b97a9416161e38044e0",
+    "url": "/Air-War/static/media/snow_scuffed_ground_1.8cb62751.jpg"
+  },
+  {
+    "revision": "030cb7e826794a60fb6ae417b8fb3a4b",
+    "url": "/Air-War/static/media/win.030cb7e8.ogg"
+  }
+]);
